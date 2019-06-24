56dd1f46e7ad91de2c0b2bbc67eaa2c86013143a
AsmaaZakaria <asmaazakaria94@gmail.com>
2019-06-24 19:14:15 +0200

    add missing semicolon & delete console log

--- 3-PhotoEditor/script.js
+++ 3-PhotoEditor/script.js
@@ -4,8 +4,8 @@ const photo = document.querySelector(".photo");
 const props = document.querySelectorAll(".props input");
 
 function handleChange() {
-    console.log('==> ', this.name, this.value);
     const suffix = this.dataset.size || "";     // will be px for all except base-color, which does not has suffix
+
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
